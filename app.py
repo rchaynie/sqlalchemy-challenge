bad8d991f6eb8e7287af5dad79c2b15f350bed8f
Reid Haynie <reidhaynie@Reids-MBP-2.lan>
2020-10-24 12:38:16 -0400

    fixed endpoints

--- app.py
+++ app.py
@@ -50,15 +50,16 @@ def home():
         f"/api/v1.0/tobs"
         f"/api/v1.0/<start>"
         f"/api/v1.0/<start>/<end>")
-    
+
+ # Design a query to retrieve the last 12 months of precipitation data and plot the results
+# Calculate the date 1 year ago from the last data point in the database
    
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
 
-    # Design a query to retrieve the last 12 months of precipitation data and plot the results
-    # Calculate the date 1 year ago from the last data point in the database
+
 
     max_date=session.query(func.max(measurement.date)).all()[0]
     
@@ -81,19 +82,55 @@ def precipitation():
     # prcp_df=prcp_df.set_index("date").sort_values("date")
     # prcp_df
 
+    # results = dict.fromkeys( {
+    #     "date": lasy_year_prcp['date'],
+    #     "precip" : lasy_year_prcp['prcp']
+    # } )
   
 
     return jsonify(lasy_year_prcp)
-    
+
+
+# What are the most active stations? (i.e. what stations have the most rows)?
+# List the stations and the counts in descending order.
 
     
 @app.route("/api/v1.0/stations")
 def stations():
-    return "test"
+    session = Session(engine)
+    
+    results_stations=session.query(measurement.station, func.count(measurement.station)).group_by(measurement.station).order_by(func.count(measurement.station).desc()).all()
+    
+    stations_all = (row for row in results_stations)
+    
+    session.close()
+
+    return jsonify(results_stations)
+
+
+# Using the station id from the previous query, calculate the lowest temperature recorded, 
+# highest temperature recorded, and average temperature of the most active station?
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    return "test"
+    
+    session = Session(engine)
+    # data=session.query(func.max(measurement.tobs), func.min(measurement.tobs), func.avg(measurement.tobs)).filter(measurement.station == 'USC00519281').all()
+    
+    
+
+    temps = {
+        "max_temp_from_station_USC00519281" : session.query(func.max(measurement.tobs)).filter(measurement.station == 'USC00519281').first(),
+        "min_temp_from_station_USC00519281" : session.query(func.min(measurement.tobs)).filter(measurement.station == 'USC00519281').first(),
+        "mean_temp_from_station_USC00519281" : session.query(func.avg(measurement.tobs)).filter(measurement.station == 'USC00519281').first()
+    }
+
+    session.close()
+
+    return jsonify(temps)
+
+
+# bonus portion
 
 @app.route("/api/v1.0/<start>")
 def api():
